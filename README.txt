dbb0046451a001bbdec4829659c259f2defe4784
Fabio Rueda <fabio@vizzuality.com>
2013-04-17 14:47:32 +0200

    pypi package

---
+++ README.txt
@@ -0,0 +1,6 @@
+============================================
+DynamoDB Foreign Data Wrapper for postgresql
+============================================
+
+This data wrapper allows to do 'select *' queries on a DynamoDB database (NoSQL)
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+#from distutils.core import setup
+from setuptools import setup
+import os 
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='DynamodbFdw',
+    version='0.1.1',
+    author='Fabio Rueda',
+    author_email='avances123@gmail.com',
+    packages=['dynamodbfdw'],
+    scripts=['bin/install-multicorn-9.1.sh'],
+    url='https://github.com/avances123/dynamodb_fdw',
+    license='LICENSE.txt',
+    description='Postgresql Foregin Data Wrapper mapping Amazon DynamoDB',
+    #long_description=open('README.txt').read(),
+    #long_description=read('README.txt'),
+    install_requires=["boto"],
+)
